68dbcd3dfc4316f01380ea03c309a0854e9ba2d4
shikun <kunish.butt@gmail.com>
2021-08-22 04:52:15 +0800

    feat: add .hyper.js

---
+++ stuff/dotfiles/.hyper.js
@@ -0,0 +1,16 @@
+module.exports = {
+  config: {
+    verminal: {
+      fontFamily: '"Cascadia Mono", "Roboto Mono for Powerline"',
+      fontSize: 12,
+    },
+    padding: "0 10px 0",
+    bell: false,
+    defaultSSHApp: false,
+    shell: "/usr/local/bin/zsh",
+    modifierKeys: {
+      altIsMeta: true,
+    },
+  },
+  plugins: ["verminal"],
+};
