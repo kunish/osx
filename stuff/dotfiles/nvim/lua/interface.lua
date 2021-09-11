2eeadc0ed4d9a8185d59be2579ccc9f4ea7f28dd
shikun <kunish.butt@gmail.com>
2021-09-11 18:54:34 +0800

    feat: add more telescope keymaps

--- stuff/dotfiles/nvim/lua/interface.lua
+++ stuff/dotfiles/nvim/lua/interface.lua
@@ -14,14 +14,11 @@ vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 
-vim.api.nvim_set_keymap("n", "<leader>fl", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
 --
 
 -- barbar
-vim.g.bufferline = {
-	animation = false,
-	auto_hide = true,
-}
+vim.g.bufferline = { animation = false, auto_hide = true }
 
 vim.api.nvim_set_keymap("n", "<Tab>", "<cmd>BufferNext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<S-tab>", "<cmd>BufferPrevious<CR>", { noremap = true, silent = true })
--- stuff/dotfiles/nvim/lua/tools.lua
+++ stuff/dotfiles/nvim/lua/tools.lua
@@ -1,9 +1,7 @@
-require("telescope").setup({})
-require("hop").setup({})
+require("telescope").setup()
+require("hop").setup()
 require("nvim_comment").setup()
-require("nvim-autopairs").setup({
-	check_ts = true,
-})
+require("nvim-autopairs").setup({ check_ts = true })
 
 -- emmet
 vim.g.user_emmet_leader_key = "<C-Z>"
@@ -11,15 +9,20 @@ vim.g.user_emmet_leader_key = "<C-Z>"
 
 local noremap_opts = { noremap = true, silent = true }
 -- telescope
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", noremap_opts)
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", noremap_opts)
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>bs", "<cmd>Telescope current_buffer_fuzzy_find<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>fa", "<cmd>Telescope builtin<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>fb", "<cmd>Telescope buffers<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>ff", "<cmd>Telescope find_files<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>fg", "<cmd>Telescope git_status<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>fl", "<cmd>Telescope file_browser<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>fm", "<cmd>Telescope man_pages<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader>fs", "<cmd>Telescope live_grep<CR>", noremap_opts)
 --
 
 -- hop
-vim.api.nvim_set_keymap("n", "<leader><leader>j", "<cmd>HopLineStart<CR>", noremap_opts)
-vim.api.nvim_set_keymap("n", "<leader><leader>w", "<cmd>HopWord<CR>", noremap_opts)
-vim.api.nvim_set_keymap("n", "<leader><leader>s", "<cmd>HopChar1<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader><Leader>j", "<cmd>HopLineStart<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader><Leader>w", "<cmd>HopWord<CR>", noremap_opts)
+vim.api.nvim_set_keymap("n", "<Leader><Leader>s", "<cmd>HopChar1<CR>", noremap_opts)
 --
 
 -- incsearch
