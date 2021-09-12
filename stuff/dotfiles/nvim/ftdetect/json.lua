fbf307ab26c8230a6ec0360c1e22f3818e047a64
shikun <kunish.butt@gmail.com>
2021-09-13 05:24:23 +0800

    feat: add jsonc support

--- stuff/dotfiles/nvim/ftdetect/json.lua
+++ stuff/dotfiles/nvim/ftdetect/json.lua
@@ -1,5 +1,6 @@
 vim.cmd([[
   augroup json
-    autocmd BufRead,BufNewFile .prettierrc,.babelrc,.releaserc setfiletype json
+    autocmd BufRead,BufNewFile .prettierrc,.babelrc,.releaserc set filetype=json
+    autocmd BufRead,BufNewFile tsconfig.json set filetype=jsonc
   augroup end
 ]])
