95dfd69a3e52d7d4c2c17f108008079b3b556a4c
shikun <kunish.butt@gmail.com>
2021-09-13 09:01:16 +0800

    feat: add lua lspconfig

--- stuff/dotfiles/nvim/lua/ide.lua
+++ stuff/dotfiles/nvim/lua/ide.lua
@@ -138,6 +138,36 @@ lspconfig.tsserver.setup({
 	},
 })
 
+local sumneko_root_path = vim.env.HOME .. "/.sdk/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/" .. "macOS" .. "/lua-language-server"
+
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require("lspconfig").sumneko_lua.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+				path = runtime_path,
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	signs = true,
 	underline = true,
