78a5c80206ae0d483349b6226466180cbda429e8
shikun <kunish.butt@gmail.com>
2021-09-14 12:34:43 +0800

    feat: add nvim-cmp keymaps

--- stuff/dotfiles/nvim/lua/ide.lua
+++ stuff/dotfiles/nvim/lua/ide.lua
@@ -234,9 +234,10 @@ function ide.setup()
 			end,
 		},
 		mapping = {
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-n>"] = cmp.mapping.select_next_item(),
+			["<C-d>"] = cmp.mapping.scroll_docs(4),
+			["<C-u>"] = cmp.mapping.scroll_docs(-4),
 			["<C-j>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.close(),
 			["<CR>"] = cmp.mapping.confirm({
 				behavior = cmp.ConfirmBehavior.Replace,
 				select = true,
