6b8349497427b685f1623558973f8a56bf949355
shikun <kunish.butt@gmail.com>
2021-09-17 11:26:40 +0800

    feat: support .huskyrc

--- stuff/dotfiles/nvim/ftdetect/json.lua
+++ stuff/dotfiles/nvim/ftdetect/json.lua
@@ -1,4 +1,4 @@
 vim.cmd([[
-  autocmd BufRead,BufNewFile .prettierrc,.babelrc,.eslintrc,.releaserc set filetype=json
+  autocmd BufRead,BufNewFile .prettierrc,.babelrc,.eslintrc,.releaserc,.huskyrc set filetype=json
   autocmd BufRead,BufNewFile tsconfig.json set filetype=jsonc
 ]])
